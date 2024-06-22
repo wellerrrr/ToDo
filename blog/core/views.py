e72cf2d1d4f33b3f90579712f0f4f7bdd904714e
JustParseUser <maximboom6@gmail.com>
2024-06-22 17:54:57 +0300

    Initial commit

--- blog/core/views.py
+++ blog/core/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from config import TG_TOKEN
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    return render(request, 'core/index.html', {'tg_admin': TG_TOKEN})
 
 
 def admin(reqeust):
